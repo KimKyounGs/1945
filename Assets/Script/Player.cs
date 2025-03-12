c615e7a905ccd81e7607a429df0f50b7b878008c
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2025-03-12 16:18:32 +0900

    Player 애니메이션 적용 및 아이템 물리 머티리얼 적용
    Player Left, Right, Up 애니메이션 적용
    아이템 Physics Material 적용해서 화면상에 바운스 하게 만듦

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,8 +6,11 @@ public class Player : MonoBehaviour
     private Vector2 minBounds;
     private Vector2 maxBounds;
 
+    Animator ani; //애니메이터를 가져올 변수
+
     void Start()
     {
+        ani = GetComponent<Animator>();
         // 화면의 경계를 설정
         Camera cam = Camera.main;
         Vector3 bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, 0));
@@ -15,6 +18,7 @@ public class Player : MonoBehaviour
 
         minBounds = new Vector2(bottomLeft.x, bottomLeft.y);
         maxBounds = new Vector2(topRight.x, topRight.y);
+        
     }
 
     void Update()
@@ -22,6 +26,30 @@ public class Player : MonoBehaviour
         // 플레이어 이동
         float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        // -1   0   1
+        if (Input.GetAxis("Horizontal") <= -0.05f)
+            ani.SetBool("left", true);
+        else
+            ani.SetBool("left", false);
+
+
+        if (Input.GetAxis("Horizontal") >= 0.05f)
+            ani.SetBool("right", true);
+        else
+            ani.SetBool("right", false);
+
+
+        if(Input.GetAxis("Vertical")>=0.5f)
+        {
+            ani.SetBool("up", true);
+        }
+        else
+        {
+            ani.SetBool("up", false);
+        }
+
+
 
         Vector3 newPosition = transform.position + new Vector3(moveX, moveY, 0);
 
